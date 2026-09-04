8ddf04aeb0419de631f1c37d461968b7fd757f83
Ahmet Cilingir <face4forev@icloud.com>
2026-09-04 14:18:51 +0200

    Initial Commit

---
+++ DayPlanTests/DayPlanTests.swift
@@ -0,0 +1,17 @@
+//
+//  DayPlanTests.swift
+//  DayPlanTests
+//
+//  Created by Ahmet CILINGIR on 04.09.26.
+//
+
+import Testing
+@testable import DayPlan
+
+struct DayPlanTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
